840b9dd924c3249dc2011e8beab12df2f63f0b28
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-10 16:28:41 -0300

    Actualización

--- OrdenamientoBurbujaMejorado.c
+++ OrdenamientoBurbujaMejorado.c
@@ -1,12 +1,15 @@
-void ordenar(intv[],int n)
+void ordenar(int v[],int n)
 {
-  int i,j,aux;
-  for(i=0;i<n-1;i++)
+  int i,j,aux, ordenado=0;
+  while(ordenado==0);
+  ordenado=1;
     for(j=0;j<n-1-i;j++) //se hace esto porque tiene que cambiar a como avanzan las comparaciones del burujeo
       if(v[j]>v[j+1])
       {
+        ordenado=0;
         aux=v[j];
         v[j]=v[j+1];
         v[j+1]=aux;
       }
+      i++;
 }
