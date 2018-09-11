88b206dd3276a269bc3f4aadc9c5cfde50473ee4
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-11 15:55:11 -0300

    Ejemplo Estructuras

---
+++ EjemplosBasicos/EjercicioEstructura.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+struct fecha {int d,m,a;};
+void cargar(struct fecha *); //prototipo
+void mostrar(struct fecha);
+void dif fecha(struct fecha*r,
+               struct fecha f1,
+               struct fecha f2);
+
+int main (void)
+{
+  struct fecha f1,f2,result;
+  cargar(&f1);cargar(&f2);
+  mostrar(f1);
+  mostrar(f2);
+  dif fecha(&result,f1,f2);
+  mostrar(result);
+  return 0;
+}
+
+void cargar(struct fecha * f) //f es un puntero en la estructura Fecha
+{
+  do {
+    printf("Fecha (d/m/a): ");
+    scanf("%d/%d/%d/",&f->d,&f->m,&f->a); //para acceder a una indirección
+  } while(!esFechaGregoriana(*f));
+}
+
+void mostrar(struct fecha f)
+{
+  prinf("%02d-%02d-%4d\n",f.d,f.m,f.a);
+}
