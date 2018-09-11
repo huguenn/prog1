03f180fdb3c58f3ad1248456e1cab812fc626476
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-11 15:38:13 -0300

    Ejemplo Basico de Estructura

--- EjemplosBasicos/Estructuras.c
+++ EjemplosBasicos/Estructuras.c
@@ -7,4 +7,4 @@ int main(void)
   scanf("%d"/"%d"/"%d", &hoy.dia, &hoy.mes, &hoy.ano); //se pone el & para que sepa en que dirección guardarlo
   printf("%02d/%02d/%4d",hoy.dia,hoy.mes,hoy.ano,);// no se pone el 0 para que no haga 0018
   return 0;
-} 
+}
