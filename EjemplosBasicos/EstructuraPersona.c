60a5d9f32556cf46a94f81186564b1ff253b0fd6
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-11 19:27:33 -0300

    Ejemplo Persona Con problemas

--- EjemplosBasicos/EstructuraPersona.c
+++ EjemplosBasicos/EstructuraPersona.c
@@ -1,62 +1,63 @@
-/* VM alta baja y modificaciones de datos
-Agenda
-Lista socios
-*/
-
+// structuras abm de estructuras
+#define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #define MAX 100
-#define ALTA  1
-#define BAJA  2
+#define ALTA 1
+#define BAJA 2
 #define MODIF 3
 #define LISTA 4
 #define SALIR 5
-#define LNOM  32
-#define LTEL  16
-
-
+#define LNOM 32
+#define LTEL 16
 struct fecha
 {
-  int d,m,a;
-};
-struct persona
-{
-  char nombre[LNOM];
-  char tel[LTEL]; //no utilizar variables numericas.
-  int dni;
-  struct fecha nacim;
+	int d,m,a;
 };
 
-/* Prototipos */
+struct persona
+{
+	char nombre [LNOM];
+	char tel [LTEL];
+	int dni;
+	struct fecha nacim;
+};
+
+int menu();
+int altas (struct persona l[], int cant);
+int ingresarCadena(char cad[],int tam,char mensaje[]);
+int buscar(struct persona l[], int desde, int hasta, char busc[]);
+int bajas (struct persona l[], int cant);
+void ordenar(struct persona l[], int n);
+void sacarEnterCola(void);
+
 
 int main (void)
 {
-  struct persona alu[MAX]; //Alu es un vector porque tiene los [] de MAX elementos
-  int cant=0 // lleva registro de cuantas personas están en el vector
-  int cant=0,opcion;
-
-      while((opcion=menu())!=SALIR) //llama a menu se fija si es distinto de opción y si no es sigue. Se usa asi para no olvidarnos de ponerlo al final del while
-//los parentesis hacen que el compilador no se queje por el = ya que es de asignación cuando estamos haciendo comparaciones
-    {
-      switch(opcion)
-      {
-        case ALTA: //Esto es gracias al define que se hizo al principio. Es para que sea mas claro
-                  cant=altas(alu,cant); //va a pedir todos los datos y un enter da como finalizada la carga. Le paso el vector en cual cargar. además le decimos la cantidad que tenemos con cant. La  funcin altas devuelve cant
-                  ordenar(alu,cant); //para ordenar el vector
-                  break; //para parar el case
-        case BAJA:
-                  cant=bajas(alu,cant); //bajas no desordena
-                  break;
-        case MODIF:
-                  modifica(alu,cant);
-                  ordenar(alu,cant);
-                  break;
-        case LISTA:
-                  listar(alu,cant);
-
-      }
-    }
-    return 0;
+	struct persona alu[MAX];
+	int cant=0, opcion;
+	while ((opcion =menu())!=SALIR)
+	{
+		switch (opcion)
+		{
+			case ALTA:
+			printf("hola entraste\n");
+			cant=altas(alu,cant);
+			ordenar(alu,cant);
+			break;
+			case BAJA:
+			cant=bajas(alu,cant);// bajas no desordena por que se mueve todo uno para arriba.
+			break;
+			case MODIF:
+			//modificar(alu,cant);
+			ordenar(alu,cant);// si hace falta
+			break;
+			case LISTA:
+			//listar(alu,cant);
+			break;
+		}
+	}
 }
 
 int menu(void)
@@ -75,49 +76,59 @@ int menu(void)
   return opc;
 }
 
-void sacarEnterCola(void)
-{
-    while(fgetc(stdin)!='\n');
-    ;
-}
 
+int altas (struct persona l[], int cant)
+{
+  char nom[LNOM];
+  ingresarCadena(nom,LNOM,"Nombre: ");
+  while(cant <MAX && strlen(nom)>0)
+  {
+    strcpy(l[cant].nombre,nom);
+    ingresarCadena(l[cant].tel,LTEL,"TE: ");
+    printf("DNI: ");
+    scanf("%d", &l[cant].dni);sacarEnterCola();
+    printf("Fecha nac(d/m/a)");
+    scanf("%d/%d/%d",&l[cant].nacim.d, &l[cant].nacim.m,&l[cant].nacim.a);
+    fgetc(stdin);
+    cant++;
+    if(cant<MAX)
+      ingresarCadena(nom,LNOM,"Nom: ");
+    }
+    return cant;
+}
+int ingresarCadena(char cad[],int tam,char mensaje[])
+{
+	int i;
+	printf("%s",mensaje );
+	fgets (cad,tam,stdin);
+	i=0;
+	while (cad[i]!='\n'&& cad[i] !='\0')
+	i++;
+	if (cad[i]=='\0')
+		while(fgetc(stdin)!='\n');
+	else
+		cad[i]='\0';
+	return i;
+}
 int buscar(struct persona l[], int desde, int hasta, char busc[])
 {
     while(desde<hasta && strstr(l[desde].nombre,busc)==NULL)
         desde++;
     return desde<hasta?desde:-1; //si desde hasta es verdadero devuelve desde sino devuelve -1
 }
-
-int bajas(struct persona l[],int cant)
-{
-    char nom[LNOM]; int opc,j,desde;
-    ingresarCadena(nom,LNOM,"Nom buscado: ");
-    while(cant>=0&&strlen(nom)>0)
-    {
-        desde=buscar(l,0,cant,nom);
-        while(desde!=-1)
-        {
-            printf("%s: ",l[desde].nom);
-            printf("Desea darlo de baja? (S/N) ");
-        }
-    }
-}
-
-
-
 int bajas (struct persona l[], int cant)
 {
 	int opc,j,desde;
 	char nom[LNOM];
-	ingresarCadena(nom,LNOM,"Nom: ");
+	ingresarCadena(nom,LNOM,"Nom Buscado: ");
 	while (cant>=0&&strlen(nom)>0)
 	{
-		desde=buscar (l,0,cant,nom);
-		while(desde !=-1)
+		desde=buscar(l,0,cant,nom);
+		while(desde!=-1)
 		{
-			printf("%s"l[desde].nom);
+			printf("%s", l[desde].nombre);
 			printf("desea darle la baja?(S/N)\n");
-			scanf("%d",&opc);
+			scanf("%c",&opc);
 			sacarEnterCola();
 			if(toupper(opc)=='S')
 			{
@@ -134,59 +145,26 @@ int bajas (struct persona l[], int cant)
 	}
 	return cant;
 }
-
-
-
 void ordenar(struct persona l[], int n)
 {
-	int ordenado = 0, i = 0, j;
-	struct persona aux;
-	while (!ordenado) //mientras no este ordenado
-	{
-		ordenado = 1; //cambio el flag para salir del while si no hay que hacer cambios
-		for (j = 0; j < n - 1 - i; j++) // n-1 por burbujeo ya que compara en 5 elementos vs 4. y se le resta i ya que al llegar al fondo el ordenamiento no toca más al más grande.
-			if (strcmp(l[j].nombre, l[j + 1].nombre) > 0)
-			{
-				ordenado = 0; //cambia flag ya que hay cambios
-				aux = l[j]; //guardo en auxiliar
-				l[j] = l[j + 1]; //guardo en l (en indice j)
-				l[j + 1] = aux; //guardo en l con j indice + 1
-			}
-		i++; //aumento el i para cumplir con lo comentado en for.
-	}
-}
-
-int altas (struct persona l[], int cant)
-{
-  char nom[LNOM];
-  ingresarCadena(num,LNOM,"Nonmbre: ");
-  while(cant <MAX && strlen(nom)>0)
-  {
-    strcpy(l[cant].nombre,nom);
-    ingresarCadena(f[cant].tel,LTEL,"TE: ");
-    printf("DNI: ");
-    scanf("%d", &l[cant].dni);sacarEnterCola();
-    print("Fecha nac(d/m/a)");
-    scanf("%d/%d/%d", &l[cant].nacim.d,&l[cant].nacim.m,&l[cant].nacim.a);
-    fgetc(stdin);
-    cant++;
-    if(cant<MAX)
-      ingresarCadena(nom,LNOM,"Nom: ");
+    int ordenado=0,i=0,j;
+    struct persona aux;
+    while(!ordenado)
+    {
+        ordenado=1;
+        for(j=0;j<n-1-i;j++)
+            if(strcmp(l[j].nombre,l[j+1].nombre)>0)
+            {
+                ordenado=0;
+                aux=l[j];
+                l[j]=l[j+1];
+                l[j+1]=aux;
+            }
+        i++;
     }
-    return cant;
 }
-
-int ingresarCadena(char cad[],int tam,char mensaje[])
+void sacarEnterCola(void)
 {
-	int i;
-	printf("%s",mensaje );
-	fgets (cad,tam,stdin);
-	i=0;
-	while (cad[i]!='\n'&& cad[i] !='\0')
-	i++;
-	if (cad[i]=='\0')
-		while(fgetc(stdin)!='\n');
-	else
-		cad[i]='\0';
-	return i;
+    while(fgetc(stdin)!='\n');
+    ;
 }
