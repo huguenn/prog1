af0aa507e933545c1341d763873f60fa64136e14
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-10 22:33:50 -0300

    Actualización

--- CambiarDiagonales.c
+++ CambiarDiagonales.c
@@ -1,71 +1,50 @@
-#define C 5
-#define F 4
-#define SUP 999 // Para srand
-#define INF 100 // para srand
-void cargar(int m[F][C]);
-void mostrar(int m[F][C]);
-void orden(int m[F][C]);
-void mostrarorden(int m[F][C]);
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int main(void)
+#include  <stdio.h>
+#include <stdlib.h>
+
+int main ()
 {
-	int m[F][C];
-	srand((unsigned int)time(NULL));
-	cargar(m);
-	mostrar(m);
-	orden(m);
-	printf("\n");
-	mostrar(m);
-  printf("\n");
-  intercambiarDiagonal(m);
-  mostrar(m);
+
+	int **array;
+	int n, i, j, aux, contador=0;
+	//Solicito el tamaño del array
+	printf ("Introduce el tamanio del array: ");
+	scanf ("%d",&n);
+	//Reservo la memoria para el array de NxN
+	array=(int**)malloc (n*sizeof (int*));
+	for (i=0;i<n;i++)
+		array[i]=(int*)malloc (n*sizeof (int));
+
+	//Relleno el array
+	for (i=0;i<n;i++){
+		for (j=0;j<n;j++){
+			contador++;
+			array[i][j]=contador;
+		}
+	}
+
+	//Muestro el array
+
+	for (i=0;i<n;i++){
+		for (j=0;j<n;j++){
+			printf ("%4i", array[i][j]);
+		}
+		printf ("\n");
+	}
+	printf ("\n");
+
+	//Intercambio las diagonales
+	for (i=0, j=n-1; i<n && j>=0;i++, j--){
+		aux=array[i][i];
+		array[i][i]=array[i][j];
+		array[i][j]=aux;
+	}
+
+	//Muestro el array
+	for (i=0;i<n;i++){
+		for (j=0;j<n;j++){
+			printf ("%4i", array[i][j]);
+		}
+		printf ("\n");
+	}
 	return 0;
-}
-
-void cargar(int m[F][C])
-{
-	int f, c; //Declaro mis variables para la carga de la matriz
-	for (f = 0; f < F; f++)
-	{
-		for (c = 0; c < C; c++)
-		{
-			m[f][c] = rand() % (SUP - INF + 1) + INF; //cargo la fórmula para los rand, se suma el INF para que no arranque en cero
-		}
-	}
-}
-
-
-void mostrar(int m[F][C])
-{
-	int f, c;
-	for (f = 0; f < F; f++)
-	{
-		for (c = 0; c < C; c++)
-		{
-			printf(" %4d", m[f][c]);
-		}
-			printf("\n");
-
-	}
-}
-
-void intercambiarDiagonal(int m[F][C])
-{
-  int f, c, col,aux;
-  for (f = 0; f < F; f++)
-  {
-    for (c = 0; c < C; c++)
-    {
-      for(f=0; f<F; f++)
-      {
-          col = f;
-          temp = A[f][col];
-          A[f][col] = A[f][(size-col) - 1];
-          A[f][(C-col) - 1] = aux;
-      }
-
-
-    }
 }
---
+++ LlenarMatriz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SUP 999
+#define INF 100
+#define FIL 4
+#define COL 5
+
+void cargar(int matriz[FIL][COL]);
+void mostrar(int matriz[FIL][COL]);
+int main (void)
+{
+  int matriz[FIL][COL];
+  int m[FIL][COL]={{0}};
+//  cargar(matriz);
+  mostrar(m);
+  return 0;
+}
+
+/*
+void cargar(int matriz[FIL][COL])
+{
+  int f,c;
+  for (f=0;f<FIL;f++)
+    for (c=0;c<COL;c++)
+      matriz[f][c]=rand()%(SUP-INF+1)+INF;
+}
+*/
+
+void mostrar(int m[FIL][COL])
+{
+  int f, c;
+  for (f=0;f<FIL;f++)
+  {
+    for(c=0;c<COL;c++)
+      printf("%4d", m[f][c]);
+    printf("\n");
+  }
+}
