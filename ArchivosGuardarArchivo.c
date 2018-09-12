2c285d3f784f58968c9569a805f2cc4842c64905
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-12 18:40:35 -0300

    actualización guardar archivos

--- ArchivosGuardarArchivo.c
+++ ArchivosGuardarArchivo.c
@@ -1,7 +1,7 @@
-void guardarArchivos(struct persona alu[], int cant)
+void guardararchivos(struct persona alu[], int cant)
 {
 	FILE *pf;
-	*pf = fopen("lista.dat", "wb"); //abro el archivo lista.dat donde voy a escribir (para guardarlo) y es binario por el .dat
+	pf = fopen("lista.dat", "wb"); //abro el archivo lista.dat donde voy a escribir (para guardarlo) y es binario por el .dat
 	if (pf) //Si pude abrir el archivo quiere decir esto. Si pude, voy a guardar mis datos en el archivo.
 	{
 		fwrite(alu, sizeof(struct persona), cant, pf); //le paso: donde esta la info a guardar(le paso el vector, es decir, la dirección del vector, le paso el tamaño, para eso uso SIZEOF, le paso la cantidad de registros que los saco de CANT Y por último el archivo pf
