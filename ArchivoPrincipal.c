9cb3614e088aacfb7f2e031d97fb989bd8171199
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-12 17:30:58 -0300

    Agregado FileSize

--- ArchivoPrincipal.c
+++ ArchivoPrincipal.c
@@ -1,13 +1,30 @@
 /*
 sizeof(struct persona) es un tipo de dato y es lo importante a la hora de copiar los datos de memoria. Esto es lo que se pone a la hora del size of.
 sizeof(struct persona alu[]) seria el tamaño de un puntero
+
+Leer Arch
+
+para leer el archivo hay que
+
+fileSize
+
+fseek mueve desde y hasta donde yo quiero pero tengo que tener cuidado de no pasarme del fin del archivo ni del principio.
+parametros
+*primer parametro para quien
+*cuanto me quiero mover
+*SEEKSET||SEEKCUR||SEEKEND en seek set se pueden poner numeros positivos y en end negativos, porque nos movemos del inicio para adelante en el caso de seekset y del fin para atras en el caso de
+seek end. Si ponemos un valor positivo en seekend nos pasariamos del archivo y está mal.
+Ejemplo
+fseek(pf,oL,SEEKEND);
+
+L
 */
 
 int main(void)
 {
   struct presona alu[MAX];
-  int cant=0,opc;
-    leerArch()
+  int cant,opc;
+    cant=leerArch();
   while((opc=menu())!=SALIR)
   {
     switch(opc)
@@ -19,6 +36,38 @@ int main(void)
   return 0;
 }
 
+
+/*
+La logica de esto es enviar a fseek al final y preguntar con ftell donde está.
+*/
+int fileSize(FILE *pf)
+{
+  long int posAct,posFin;
+  postAct=ftell(pf); //ftell devuevle un long int
+  fseek(pf,0L,SEEKEND);
+  posFin=ftell(pf);
+  fseek(pf,postAct,SEEKSET);
+  return posFin;
+}
+
+
+
+int leerArch(struct persona alu[])
+{
+  FILE *pf=fopen("Lista.dat","rb");
+  int cant=0; //si no puede abrirlo o hay algun problema que el programa sepa que no pudo mediante un 0
+  if(pf)
+  {
+    cant=filesize(pf)/sizeof(struct persona); //con esto saco la cantidad de registros
+    fread(alu,sizeof(struct persona),cant,pf); //
+    fclose(pf);
+  }
+  else
+  {
+    print("Error\n")
+  }
+  return cant;
+}
 
 void guardarArchivo(struct persona alu[], int cant)
 {
