97c7dae3d266aee9a0d9b9c52f45f6cc7547c70f
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-12 22:27:55 -0300

    Contar Palabras TXT

--- ArchivoContarPalabrasTxt.c
+++ ArchivoContarPalabrasTxt.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-
-#define TAM 100
-typedef enum { FALSO, VERDAD } bool; //creamos el booleano donde typedef enum designa 0 para el primer valor y 1 para el segundo, 3 para el tercero y así sucesivamente
+#define LEER_ARCHIVO "Origen.txt"
+typedef enum { FALSO, VERDAD } boolean; //creamos el booleano donde typedef enum designa 0 para el primer valor y 1 para el segundo, 3 para el tercero y así sucesivamente
 //en este caso para que FALSO sea 0 y VERDADERO 1 se coloca primero FALSO y después VERDADERO
 
 bool EsSeparador(char); //Prototipo para EsSeparador
@@ -10,16 +9,21 @@ bool EsSeparador(char); //Prototipo para EsSeparador
 int main(void)//Inicio función main ******************************
 {
 	bool EnPalabra = FALSO; //Inicializamos en Palabran en FALSO que gracias a lo que hicimos al principio es equivalente a 0
-	char cad[TAM]; //Declaramos el vector/string con tamaño TAM
-	int i = 0, cantPal=0; //Declaramos el contador i e inicializamos cantPal en 0 para arrancar la cuenta en 0
+    FILE * pf;
+    int cantPal = 0;
+    int cantLin = 0;
+    char c;
+    pf=fopen(LEER_ARCHIVO, "rt");
+    if(pf==NULL)
+    {
+        printf("No se puede abrir el archivo \n");
+        return 0;
+    }
 
-	printf("Ingresar texto: ");
-	fgets(cad, TAM, stdin); //Utilizamos fgets para obtener los datos de teclado (stdin) con un tamaño TAM y en vector/string cad
-	while (cad[i] != '\0') //Loop para que se repita mientras el valor obtenido no sea null (gracias a fgets siempre tendremos un \0 cuando se termine de ingresar los datos)
-	{
+    while((c=fgetc(pf)) != EOF) {
 		if (!EnPalabra) //Hacer esto cuando EnPalabra sea FALSO es decir si no está en palabra hay que hacer buscar si el caracter es un separador
 		{
-			if (!EsSeparador(cad[i]))/* inicio palabra - Entramos en la supuesta palabra, utilizando la función EsSeparador vemos si el caracter es un separador */
+			if (!EsSeparador(c))/* inicio palabra - Entramos en la supuesta palabra, utilizando la función EsSeparador vemos si el caracter es un separador */
 			{
 				EnPalabra = VERDAD; // en caso que lo sea, aplicamos VERDAD
 				cantPal++; //Incrementamos el contador de palabras
@@ -27,12 +31,11 @@ int main(void)//Inicio función main ******************************
 		}
 		else
 		{
-			if (EsSeparador(cad[i])) /* fin palabra - Volvemos a usar EsSeparador para verificar que lo que tenemos como caracter sea un separador */
+			if (EsSeparador(c)) /* fin palabra - Volvemos a usar EsSeparador para verificar que lo que tenemos como caracter sea un separador */
 			{
 				EnPalabra = FALSO;  //Asignamos un FALSO y volvemos a empezar
 			}
 		}
-		i++; //salimos del if e incrementamos para seguir comparando
 	}
 	printf("Cantidad de palabras: %d\n", cantPal);  //mostramos la magia de cuantas palabras obtenemos
 	return 0; //Importante. debe devolver algo porque main tiene un int en la declaración
@@ -45,5 +48,5 @@ bool EsSeparador( char c)  //Inicio funcion EsSeparador, con un char de nombre c
   {
   	return VERDAD; //si lo encontramos es decir que strchr devuelve un 1 por ser TRUE devolvemos un VERDADERO
   }
-  return FALSO; //si no se cumple devolvemos un falso, esto tambien podría ser un else, pero no es necesario. 
+  return FALSO; //si no se cumple devolvemos un falso, esto tambien podría ser un else, pero no es necesario.
 }
