d91d5bae03ecda937b68222033791627bb48808c
Hugo Andrés Vázquez <hvazquez@ar.ibm.com>
2018-09-13 22:12:04 -0300

    Actualización Archivo contar palabras txt

--- ArchivoContarPalabrasTxt.c
+++ ArchivoContarPalabrasTxt.c
@@ -5,6 +5,7 @@ typedef enum { FALSO, VERDAD } boolean; //creamos el booleano donde typedef enum
 //en este caso para que FALSO sea 0 y VERDADERO 1 se coloca primero FALSO y después VERDADERO
 
 bool EsSeparador(char); //Prototipo para EsSeparador
+bool EsNumero(char);
 
 int main(void)//Inicio función main ******************************
 {
@@ -26,6 +27,7 @@ int main(void)//Inicio función main ******************************
 		i=0
     while(!feof(pf))
 		{  //Fin Agregado para que funcione con archivos. Preguntar si el EOF está bien usado/es una buena practica
+
 			if (!EnPalabra) //Hacer esto cuando EnPalabra sea FALSO es decir si no está en palabra hay que hacer buscar si el caracter es un separador
 			{
 				if (!EsSeparador(c))/* inicio palabra - Entramos en la supuesta palabra, utilizando la función EsSeparador vemos si el caracter es un separador */
@@ -56,6 +58,16 @@ bool EsSeparador( char c)  //Inicio funcion EsSeparador, con un char de nombre c
 {
 	static char separadores[] = " ,.-+*/#&=;:_\"\'?!()[]{}\n";  //Inicializamos un vector que tiene como contenido todos los separadores posibles
 	if (strchr(separadores, c))  //haciendonos de la función strchr buscamos en el vector separadores el caracter recibido como parámetro c
+  {
+  	return VERDAD; //si lo encontramos es decir que strchr devuelve un 1 por ser TRUE devolvemos un VERDADERO
+  }
+  return FALSO; //si no se cumple devolvemos un falso, esto tambien podría ser un else, pero no es necesario.
+}
+
+bool EsNumero( int c)  //Inicio funcion EsSeparador, con un char de nombre c que se recibe como parámetro
+{
+	static int numero[] = "0123456789";  //Inicializamos un vector que tiene como contenido todos los separadores posibles
+	if (strchr(numero, c))  //haciendonos de la función strchr buscamos en el vector separadores el caracter recibido como parámetro c
   {
   	return VERDAD; //si lo encontramos es decir que strchr devuelve un 1 por ser TRUE devolvemos un VERDADERO
   }
